66b9f1e341eab851b23c0012847f2502a698d2fa
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2024-01-21 12:19:12 +0530

    Content added in main component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Header from "./Header";
+import Main from "./Main";
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Main>
+        <p>Hey There !</p>
+      </Main>
     </div>
   );
 }

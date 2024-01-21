b7bf0aca871559f00c97c46ed0472d63e3c4fa96
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2024-01-21 12:53:36 +0530

    fake api fetch using fatch function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <div className="App">
       <Header />

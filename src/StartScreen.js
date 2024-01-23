458ddc079a2681e1605ccf352e5a820b017a6bb9
kuldip2002 <dev.kuldipmathukiya@gmail.com>
2024-01-23 12:36:38 +0530

    callback function added in onClick handler

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -5,7 +5,10 @@ function StartScreen({ numQuestions, dispatch }) {
     <div className="start">
       <h2>Welcome to The React Quiz !</h2>
       <h3>{numQuestions} questions to check your React Mastery !</h3>
-      <button className="btn btn-ui" onClick={dispatch({ type: "active" })}>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "active" })}
+      >
         Let's start
       </button>
     </div>
